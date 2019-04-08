b22a7494ed1b7807b8a395287e2dc77d98a56c77
gabrielArSouza <gabriel_feg@hotmail.com>
2019-04-08 16:29:41 -0300

    orthographic camera

--- include/camera.h
+++ include/camera.h
@@ -5,9 +5,48 @@
 
 class Camera {
 
-protected:
-    Ray generate_ray(int x, int y);
+private:
 
+    int width;  //<! Width of camera 
+    int height; //<! height of camera
+
+public:
+
+    /**
+     * @brief Construct a new Camera object
+     * 
+     * @param width   the width of camera
+     * @param height  the height of camera
+     */
+    Camera (int width, int height) 
+    : width{width}, height{height} {/*empty*/} 
+
+    /**
+     * @brief Destroy the Camera object
+     */
+    ~Camera(){/*empty*/}
+
+    /**
+     * @brief generate a ray based in a coordenates x and y.
+     * Each camera implements the own method
+     * 
+     * @param x   the x value
+     * @param y   the y value
+     * @return Ray  The ray in x,y pixel. 
+     */
+    virtual Ray generate_ray(int x, int y);
+
+    /**
+     * @brief Get the width object
+     * @return int  the width value
+     */
+    int get_width () {return width;}
+
+    /**
+     * @brief Get the height object
+     * @return int The height value
+     */
+    int get_height (){return height;}
 };
 
 #endif
--- include/orthographic_camera.h
+++ include/orthographic_camera.h
@@ -5,6 +5,42 @@
 
 class OrthographicCamera : public Camera {
 
+private:
+    point3 position;  //<! located at the origin
+    point3 target;    //<! looking down the -Z axis
+    Vec3 upVector;    //<! the camera's up vector
+
+    //<! View plane dimensions 
+    //<! [left, right, bottom and up]
+    float l, r, b, t;   
+
+public: 
+
+    /**
+     * @brief Construct a new Orthographic Camera object
+     * 
+     * @param w         the width of camera
+     * @param h         the height of camera
+     * @param position  the camera's position
+     * @param target    the camera's target (-Z axis)
+     * @param upVector  the camera's up vector
+     * @param l         the left view plane dimension
+     * @param r         the right view plane dimension
+     * @param b         the bottom view plane dimension
+     * @param t         the top view plane dimension
+     */
+    OrthographicCamera( int w, int h, point3 &position,
+        point3 &target, Vec3 &upVector, float l, float r,
+        float b, float t) 
+        
+        : Camera(w,h), position{position}, target{target},
+          upVector{upVector}, l{l}, r{r}, b{b}, t{t}
+    {/*empty*/}
+
+    ~OrthographicCamera(){/*empty*/}
+
+    Ray generate_ray(int x, int y);
+
 };
 
 #endif
